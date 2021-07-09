cf02a56289406492ce80a23af71bcc15d4b00bf9
Ryan Carlson <ryan.carlson@mercsystems.ca>
2021-07-09 15:37:10 -0600

    Added arrays for tweet, username, date created and age. Also added a loop to print info to console.

---
+++ w9c/js/app.js
@@ -0,0 +1,13 @@
+//Arrays
+var tweet = ["Visited a giant YMCA today, and it was awesome to be back", "Happy #permission to dance Day!", "What's cutting the Oilsands carbon emissions? A cool $75 billion", "Locked and loaded #saratoga", "Tesla Solar Roof + Powerwall in major new housing development in Austin", "Nikita Kucherov and Bud Light. What a pair.", "Today's the day: applications for fall NASA internships are due!", "The current standing at the 41st #USSeniorOpen", "Well, I guess i'm a pretty sick guy...", "Have a great, safe Calgary Stampede folks!"];
+var username = ["Giant Blue Ring", "William Shatner", "Calgary Herald", "Dave Portnoy", "Elon Musk", "Sportsnet", "NASA", "Golf Channel", "Spencer Charnas", "EHS Analytics"];
+var created_at = ["07-08-2021", "07-09-2021", "07-08-2021", "07-09-2021", "07-09-2021", "07-09-2021", "07-09-2021", "07-09-2021", "07-09-2021","07-09-2021"];
+var age = [8, 90, 13, 45, 50, 13, 63, 13, 30, 5];
+
+// for loop
+for (var tweets = 0; tweets < tweet.length; tweets++) {
+    console.log(tweet[tweets]);
+    console.log(username[tweets]);
+    console.log(created_at[tweets]);
+    console.log(age[tweets]);   
+}
